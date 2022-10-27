7cb34620e644bcc8c8d80da1ff7386fe5f000213
JeongTaehwan <wjdxoghks11222@naver.com>
2022-10-27 11:53:36 +0900

    Update : App.vue 파일 코드 수정 및 폼 추가

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form>
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" />
+    </div>
+    <button>login</button>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
